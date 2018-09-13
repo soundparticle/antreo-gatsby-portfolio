e2ab61ab6b5ac30a6f60a07931c11d01cf737f09
Antreo Pukay <acrosswalker@yahoo.com>
2018-09-13 08:45:53 -0700

    2nd initial commit

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
-import Header from "../components/header"
 
-export default () => (
-  <div style={{ color: `teal` }}>
-    <Link to="/">Home</Link>
-    <Header headerText="Contact" />
-    <p>Send us a message!</p>
-  </div>
-)
+export default props => 
+<h1>{props.headerText}</h1>
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,23 @@
 import React from "react";
-import { Link } from "gatsby";
+// import { Link } from "gatsby";
 import Header from "../components/header";
 
+// export default () => <div>Hello World</div>
+
+
+
 export default () => (
-  <div style={{ color: `purple` }}>
-  <Link to="/contact/">Contact</Link>
+  <div>
+
+    
+  {/* <Link to="/contact/">Contact</Link> */}
   <Header headerText="Hello Gatsby!" />
   <p>What a world.</p>
-  <img src="https://source.unsplash.com/random/400x200" alt="mountains with clouds trapped"/>
+  {/* <img src="https://source.unsplash.com/random/400x200" alt="mountains with clouds trapped"/> */}
+
+  
+
+  
   </div>
 )
 
