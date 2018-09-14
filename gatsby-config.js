9c8ae6c110599e7665e0010b38c1d702a069abfb
Antreo Pukay <acrosswalker@yahoo.com>
2018-09-13 18:26:36 -0700

    got first contenful image populating

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,13 @@ module.exports = {
         pathToConfigModule: `src/utils/typography.js`,
       },
     },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `wqczouphclv7`,
+        accessToken: `0cbd6acbdb17f3bcf62851a1cd9e2c492dd87888432f1debe64fe453170cdc21`,
+      },
+    },
+  
   ],
 }
--- src/components/imagesSection.js
+++ src/components/imagesSection.js
@@ -1,8 +1,33 @@
 import React from "react";
+import { StaticQuery, graphql } from "gatsby"
 
-export default props => 
-
-<div>
-  <p>{props.imagesSectionText}</p>
-  
-</div>
+export default () => (
+  <StaticQuery
+    query={graphql`
+      query {
+        allContentfulAntreoPortfolio {
+          edges {
+            node {
+              id
+              imageLinks {
+                id
+                file {
+                  url
+                  fileName
+                  contentType
+                }
+              }
+            }
+          }
+        }
+        
+      }
+    `
+}
+    render={data => (
+      <div>
+        <img src={data.allContentfulAntreoPortfolio.edges[0].node.imageLinks[1].file.url} alt="Alchemy Companion App screenshot"/>
+      </div>  
+    )}
+  />
+)
