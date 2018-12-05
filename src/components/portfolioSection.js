6f3bebc3562bc9f2538e205e0cc9e833ef23b454
Antreo Pukay <acrosswalker@yahoo.com>
2018-12-04 17:31:01 -0800

    added portfolioSection css module

--- src/components/portfolioSection.js
+++ src/components/portfolioSection.js
@@ -1,9 +1,12 @@
 import React from "react";
+import styles from "./portfolioSection.module.css"
 import ImagesSection from "../components/imagesSection";
+console.log(styles)
+
 
 export default props =>
 
-<div className="container">
+<div className="portfolio-container">
   <h4>Portfolio</h4>
   <ImagesSection imagesSectionText="image link tiles here"/>
 </div>
