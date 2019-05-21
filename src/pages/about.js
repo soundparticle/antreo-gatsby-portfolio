dbd497e28d62fe4fb0b104de209f7b1e631752cb
Antreo Pukay <acrosswalker@yahoo.com>
2019-05-20 22:47:23 -0700

    changed props to function call and updated about me info

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,15 +7,15 @@ import styles from "./index.module.css";
 
 
 export default () => (
-  <div  style={{ color: `` }}>
+  <div>
     <Header headerText1="Antreo Pukay" />
     <Header headerText2="Full-Stack Developer" />
     <Header headerText3="acrosswalker at yahoo.com" />
+    
     <div className={styles.linkWrapper}>
       <Link to="/" id={styles.link}>Home</Link>    
     </div>
-    <hr/>
-    
+  
     <AboutSection/> 
   </div>
 )
