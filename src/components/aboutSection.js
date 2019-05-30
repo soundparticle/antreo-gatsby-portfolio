52ba9ed10438294294ed462f7ae18279dd9b286e
Antreo Pukay <acrosswalker@yahoo.com>
2019-05-30 10:37:57 -0700

    fixed max sizing of image links and tightened magins of skills buttons

--- src/components/aboutSection.js
+++ src/components/aboutSection.js
@@ -6,6 +6,7 @@ export default () =>
 <div className={styles.aboutSection}>
 
   <h6>About Me</h6>
-  <p>I'm a Full-Stack JavaScript developer based in Portland, OR who
-  is passionate about collaboration and working together to realize people's vision of their product. I grew up in a tree-house along the Suwannee River in Florida, around artists and creative thinkers, so I bring a unique background to the table. I love using my tech and people skills to help others and positively impact our world. Besides being a developer, I've been a recording engineer since 2007. One highlight was engineering and mixing a song on a Grammy nominated album. Since then I've shifted towards web-development and contributing my skills to help create things that improve our world. As a developer, I love getting involved with projects that touch on these areas when I can! My biggest passion is to work collaboratively with others to create things which move people and impact our world in positive ways.</p>
+  <p>I'm a Full-Stack JavaScript developer based in Portland, OR who is passionate about collaboration and working together to realize people's vision of their product.</p>
+  <p>I grew up in a tree-house along the Suwannee River in Florida, around artists and creative thinkers, so I bring a unique background to the table. I love using my tech and people skills to help others and positively impact our world.</p>
+  <p>Besides being a developer, I've been a recording engineer since 2007. A particular highlight for me was engineering and mixing a song on a Grammy nominated album. Since then I've shifted towards web-development and contributing my skills to helping create things that improve our world. As a developer, I love getting involved with projects that touch on these areas when I can! My biggest passion and motivation is to work collaboratively with others to create things which move people and impact our world in positive ways.</p>
 </div>
