d81e38f7c9fba32875ec6575f215f9111e9a91a9
Antreo Pukay <acrosswalker@yahoo.com>
2019-07-12 20:53:04 -0700

    added Pug to dependencies to play with, fixed white space issue by deactivation right scroll, working on creating comet

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,20 +3,15 @@ import { Link } from "gatsby";
 import AboutSection from "../components/aboutSection";
 import Header from "../components/header";
 
-import styles from "./index.module.css";
+import styles from "./index.module.scss";
 
 
 export default () => (
   <div>
     <Header />
-    {/* <Header headerText1="Antreo Pukay" />
-    <Header headerText2="Full-Stack Developer" />
-    <Header headerText3="acrosswalker at yahoo.com" /> */}
-    
     <div className={styles.linkWrapper}>
       <Link to="/" id={styles.link} className={styles.glow}>Home</Link>    
     </div>
-  
     <AboutSection/> 
   </div>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,21 +4,31 @@ import Header from "../components/header";
 import PortfolioSection from "../components/portfolioSection";
 import Skills from "../components/skills";
 
-import styles from "./index.module.css";
+import styles from "./index.module.scss";
 
 console.log(styles)
 
 export default () => (
   <div className={styles.appContainer}>
-    <Header/>
+      
+    <Header />
     <div className={styles.linkWrapper}>
       <Link to="/about/" id={styles.link} className={styles.glow}>About</Link>    
     </div>
+    <svg id={styles.svgCanvas}  preserveAspectRatio="xMinYMin slice" width="100%">
+      {/* Translate was originally 150, 150 */}
+      <g transform="translate(50, 60)">
+        <circle className={styles.shootingStar} fill='white' cx="0" cy="0" r="2" radius="125" ></circle>
+      </g>
+    </svg>  
+
+
     <main>
       <div className={styles.pageContainer}>        
         <PortfolioSection/>
         <Skills/>
       </div>
     </main>
+    
   </div>  
 )
