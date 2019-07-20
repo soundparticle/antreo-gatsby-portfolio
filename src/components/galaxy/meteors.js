6b7f4f9f13f1687529bce25c271bcc0b9236404c
Antreo Pukay <acrosswalker@yahoo.com>
2019-07-19 20:55:28 -0700

    wdded meteors and meteorData componen

---
+++ src/components/galaxy/meteors.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Meteor } from './meteor';
+import { METEOR_DATA } from './meteorData';
+
+export class Meteors extends React.Component {
+  timerRef = null;
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeMeteorIndex: 0
+    }
+
+    // this.next = this,next.bind(this);
+  }
+
+  next() {
+    clearTimeout(this.timerRef);
+
+    const availableCount = METEOR_DATA.length;
+    let newIndex = Math.round(Math.random() * availableCount);
+
+    if(newIndex === this.state.activeMeteorIndex) {
+      newIndex = (newIndex + 1) % availableCount;
+    }
+
+    this.setState({
+      activeMeteorIndex: newIndex
+    })
+
+    const minDelay = 5000;
+    const maxDelay = 10000;
+    const nextTickTimeout = minDelay + Math.random() * (maxDelay = minDelay);
+
+    this.timeRef = setTimeout(this.next, nextTickTimeout);
+  }
+
+  render() {
+    const {activeMeteorIndex} = this.state;
+    const meteor = METEOR_DATA[activeMeteorIndex];
+    const {debug} = this.props;
+
+    if (debug === false) {
+      return <Meteor {...meteor} debug={false} />;
+    } else {
+      return (
+        <g >
+          {
+            METEOR_DATA.map((meteorData, index) => (
+              <Meteor {...meteorData} key={index} debug={true} />
+            ))
+          }
+        </g>
+      )
+    }
+  }
+}
