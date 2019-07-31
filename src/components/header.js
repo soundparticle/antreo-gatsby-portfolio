f3d86a8eca1d27612538778433be1bca5b56a93c
Antreo Pukay <acrosswalker@yahoo.com>
2019-07-31 14:47:26 -0700

    cleaned up some unused code and removed unused files

--- src/components/header.js
+++ src/components/header.js
@@ -8,11 +8,6 @@ export default () =>
   <header className={styles.headerContainer}>
     
     <div id={styles.headerPair}>
-      {/* <svg id={styles.svgCanvas}  preserveAspectRatio="xMinYMin slice" width="100%">
-        <g transform="translate(50, 60)">
-          <circle className={styles.shootingStar} fill='white' cx="0" cy="0" r="2" radius="125" ></circle>
-        </g>
-      </svg>   */}
       <h1 className={styles.headerOne}>ANTREO PUKAY</h1>
       <h2 className={styles.headerTwo}>Full-Stack Developer</h2>
       <h3 className={styles.headerThree}>Acrosswalker at yahoo.com</h3>
