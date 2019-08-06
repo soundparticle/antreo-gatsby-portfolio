70f64690868427324a5eff456f4460bf0099c29f
Antreo Pukay <acrosswalker@yahoo.com>
2019-08-05 18:57:03 -0700

    removed Typography, still need to format link and email

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,12 +3,6 @@ module.exports = {
   plugins: [
     `gatsby-plugin-sass`,
     {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography.js`,
-      },
-    },
-    {
       resolve: `gatsby-source-contentful`,
       options: {
         spaceId: `wqczouphclv7`,
