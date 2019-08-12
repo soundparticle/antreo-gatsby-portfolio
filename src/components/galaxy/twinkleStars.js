afe9191b690199f4fc18dd6c9351cef55ed4bde8
Antreo Pukay <acrosswalker@yahoo.com>
2019-08-12 01:27:20 -0700

    slowed down twinkleStars

--- src/components/galaxy/twinkleStars.js
+++ src/components/galaxy/twinkleStars.js
@@ -6,8 +6,8 @@ export const TwinkleStars = ({ stars, debug }) => (
   <g>
     { stars.map((coord, index) => {
       const offset = index * 100 + 500 * Math.random();
-      const duration = 2000 + Math.random() * 2000;
-      const brightness = 0.7 + Math.random() * 0.3;
+      const duration = 3000 + Math.random() * 2000;
+      const brightness = 0.7 + Math.random() * 0.5;
 
       return (
         <Star
