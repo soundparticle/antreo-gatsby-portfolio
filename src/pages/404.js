d35a3520b527c4901cb64cbd8ac8c8a022270263
Antreo Pukay <acrosswalker@yahoo.com>
2019-08-15 00:56:59 -0700

    added 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import backArrow from '../assets/back-arrow.png'; 
+
+import styles from './404.module.scss';
+
+
+class FourOhFour extends React.Component {
+  render() {
+    return (
+      <div >
+        <h1>Oh no...page not found!</h1>
+        <Link to="/">
+          <img id={styles.backArrow} src={backArrow} alt="Back Arrow"/>
+        </Link>
+      </div>
+    );
+  }
+}
+
+export default FourOhFour;
