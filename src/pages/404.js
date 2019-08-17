38afce61bc1661c810f260da76006cea42aca1f1
Antreo Pukay <acrosswalker@yahoo.com>
2019-08-17 14:19:56 -0700

    added empty 404 css file

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { Link } from 'gatsby';
 import backArrow from '../assets/back-arrow.png'; 
 
+import styles from './404.module.css';
+
 class FourOhFour extends React.Component {
   render() {
     return (
       <div>
         <h1>Oh no...page not found!</h1>
         <Link to="/">
-          <img src={backArrow} alt="Back Arrow"/>
+          <img id={styles.backArrow} src={backArrow} alt="Back Arrow"/>
         </Link>
       </div>
     );
