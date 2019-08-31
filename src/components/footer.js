ef847ff24bde93bd2775b713894ceed97bfd43b5
Antreo Pukay <acrosswalker@yahoo.com>
2019-08-31 13:27:23 -0700

    added styling to center socialNav footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,9 +3,10 @@
 import React from 'react';
 import SocialNav from '../components/socialNav';
 
+
 export default () => 
   <footer>
-    <section >
+    <section style={{ display: 'flex', justifyContent: 'center' }}>
       <SocialNav />
     </section>
   </footer>;
