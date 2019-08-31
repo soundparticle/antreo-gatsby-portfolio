47a9e4078dabd21b5fc8063d33d7b4aef6ffc9bf
Antreo Pukay <acrosswalker@yahoo.com>
2019-08-31 13:03:11 -0700

    fixed h2 formatting issue for Galaxy S5

--- src/components/header.js
+++ src/components/header.js
@@ -6,8 +6,7 @@ import styles from './header.module.scss';
 export default () => 
 
   <header className={styles.headerContainer}>
-    
-    <div id={styles.headerPair}>
+    <div id={styles.headersWrapper}>
       <h1 className={styles.headerOne}>ANTREO PUKAY</h1>
       <h2 className={styles.headerTwo}>Full-Stack Developer</h2>
       <h3 className={styles.headerThree}>Acrosswalker&#64;yahoo.com</h3>
