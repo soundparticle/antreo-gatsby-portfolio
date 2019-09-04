01b47bd04a3349c0df437b2e9342f7cd58da1ad5
Antreo Pukay <acrosswalker@yahoo.com>
2019-09-03 22:15:40 -0700

    contentOverlay showing up but need to dial in slideDown effect

--- src/components/imagesSection.js
+++ src/components/imagesSection.js
@@ -29,7 +29,19 @@ export default () => (
     render={data => (
       <div className={styles.images}>
         <a href="hhttps://alchemy-companion.herokuapp.com/#/" className={styles.imageLinks} id={styles.link1} target="_blank" rel="noopener noreferrer">
-          <img className={styles.portfolioImages} src={data.allContentfulAntreoPortfolio.edges[0].node.imageLinks[0].file.url} alt="Alchemy Companion App screenshot"/>
+          <div className={styles.content}>
+            <div className={styles.contentOverlay}>
+              <div className={styles.overlayDetails} id={styles.fadeInTop}>
+                <h6>Resource sharing App for students</h6>
+                <ul className={styles.overlayList} style={{ listStyleType: 'none', color: 'white' }}>
+                  <li>Javascript</li>
+                  <li>iTunes API</li>
+                  <li>CSS</li>
+                </ul>
+              </div>
+            </div>
+            <img className={styles.portfolioImages} src={data.allContentfulAntreoPortfolio.edges[0].node.imageLinks[0].file.url} alt="Alchemy Companion App screenshot"/>
+          </div>
         </a>
         <a href="https://github.com/soundparticle/budget-tracker" className={styles.imageLinks} id={styles.link2} target="_blank" rel="noopener noreferrer">
           <img className={styles.portfolioImages} src={data.allContentfulAntreoPortfolio.edges[0].node.imageLinks[2].file.url} alt="Budget bar graph and dollar sign"/>
