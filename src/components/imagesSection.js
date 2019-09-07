5c961a54f30b14d24a4d92b648f84ed940620445
Antreo Pukay <acrosswalker@yahoo.com>
2019-09-06 20:55:27 -0700

    updated other image portfolio links

--- src/components/imagesSection.js
+++ src/components/imagesSection.js
@@ -26,13 +26,15 @@ export default () => (
       }
     `
     }
+
+
     render={data => (
       <div className={styles.images}>
         <a href="hhttps://alchemy-companion.herokuapp.com/#/" className={styles.imageLinks} id={styles.link1} target="_blank" rel="noopener noreferrer">
           <div className={styles.content}>
             <div className={styles.contentOverlay}></div>
             <div className={styles.overlayDetails} id={styles.fadeInTop}>
-              <h6>Resource sharing App for students</h6>
+              <h6>Resource sharing App for students of coding</h6>
               <ul className={styles.overlayList}>
                 <li>Javascript</li>
                 <li>PostgresQL</li>
@@ -43,10 +45,34 @@ export default () => (
           </div>
         </a>
         <a href="https://github.com/soundparticle/budget-tracker" className={styles.imageLinks} id={styles.link2} target="_blank" rel="noopener noreferrer">
-          <img className={styles.portfolioImages} src={data.allContentfulAntreoPortfolio.edges[0].node.imageLinks[2].file.url} alt="Budget bar graph and dollar sign"/>
+          <div className={styles.content}>
+            <div className={styles.contentOverlay}></div>
+            <div className={styles.overlayDetails} id={styles.fadeInTop}>
+              <h6>Simple App to track your budget</h6>
+              <ul className={styles.overlayList}>
+                <li>React/Redux</li>
+                <li>Jest/Mocha</li>
+                <li>Node</li>
+                <li>Express</li>
+              </ul>
+            </div>  
+            <img className={styles.portfolioImages} src={data.allContentfulAntreoPortfolio.edges[0].node.imageLinks[2].file.url} alt="Budget bar graph and dollar sign"/>
+          </div>
         </a>
         <a href="https://github.com/soundparticle/goals" className={styles.imageLinks} id={styles.link3} target="_blank" rel="noopener noreferrer">
-          <img className={styles.portfolioImages} src={data.allContentfulAntreoPortfolio.edges[0].node.imageLinks[1].file.url} alt="Goal Tracker Finish Line"/>        
+          <div className={styles.content}>
+            <div className={styles.contentOverlay}></div>
+            <div className={styles.overlayDetails} id={styles.fadeInTop}>
+              <h6>This App helps you track your goals</h6>
+              <ul className={styles.overlayList}>
+                <li>React/Redux</li>
+                <li>Jest</li>
+                <li>Node</li>
+                <li>Shortid</li>
+              </ul>
+            </div>  
+            <img className={styles.portfolioImages} src={data.allContentfulAntreoPortfolio.edges[0].node.imageLinks[1].file.url} alt="Goal Tracker Finish Line"/>        
+          </div>
         </a>
       </div>  
     )}
